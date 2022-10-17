f8bc66ffbe7ac922d32390cf0513357ef1e22f18
Shin-sangwon <dnjsml10@gmail.com>
2022-10-18 00:20:33 +0900

    feat #1 : 회원가입 시 정보 검증 실패한 부분 제외하고 입력 폼에 남아있게 하기

--- 1Week_Mission/src/main/java/com/ll/ebooks/domain/member/controller/MemberController.java
+++ 1Week_Mission/src/main/java/com/ll/ebooks/domain/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.ll.ebooks.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,9 +30,10 @@ public class MemberController {
 
     @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
-    public String Join(@Valid JoinRequestDto joinRequestDto, BindingResult bindingResult) {
+    public String Join(@Valid JoinRequestDto joinRequestDto, BindingResult bindingResult, Model model) {
 
         if(bindingResult.hasErrors()) {
+            model.addAttribute("joinRequestDto", joinRequestDto);
             return "member/JoinForm";
         }
 
